85bd5a793dec3bb598c4c53cd7e4e6ce405d4d36
Cody Brannon <codybrannon@Codys-MacBook-Pro.local>
2017-04-10 07:07:38 -0400

    Front end

--- assets/js/flashcard.js
+++ assets/js/flashcard.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+    messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
+
+
 // ES6 class constructors
 // class BasicCard {
 //     constructor (front, back) {
