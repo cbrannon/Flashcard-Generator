df4b4213cd29c9a6278cff67895b302fe9463d67
Brannon <E9879847@eaton.ad.etn.com>
2017-04-11 09:31:19 -0400

    Start inquirer functionality

--- flashcards.js
+++ flashcards.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-const fs = require("fs");
-const inquirer = require("inquirer");
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // ES6 class constructors
 // class BasicCard {
@@ -28,6 +28,7 @@ function BasicCard(front, back) {
     if (this instanceof BasicCard) {
         this.front = front;
         this.back = back;
+        setCard(this, 'basic_card');
     } else {
         return new BasicCard(front, back);
     }  
@@ -39,7 +40,7 @@ function ClozeCard(fullText, cloze) {
             this.fullText = fullText;
             this.cloze = cloze;
             this.partial = this.fullText.replace(this.cloze, '...');
-
+            setCard(this, 'cloze_card');
         } else {
             console.log(cloze + ' does not exist in text')
         }
@@ -48,8 +49,63 @@ function ClozeCard(fullText, cloze) {
     }
 }
 
-const georgeWashington = BasicCard("Who had wooden teeth?", "George Washington");
-const georgeCloze = ClozeCard("George Washington was old", "George Washington");
+function getCards() {
+    fs.readFile('cards.json', 'utf8', function (err, data) {
+        console.log( data );
+        // res.end( data );
+    });
+}
+
+function setCard(card, type) {
+    fs.readFile( 'cards.json', 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        if (type == 'cloze_card') {
+            data.cloze_cards.push(card);
+        } else {
+             data.basic_cards.push(card);
+        }
+        data.cloze_cards.push(card);
+        fs.writeFile( 'cards.json', JSON.stringify(data));
+        // console.log( data );
+        // res.end( JSON.stringify(data));
+    });
+}
+
+function inquireCommand() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Get cards", "Create Basic Card", "Create Cloze Card", "Delete Cards"],
+            name: "choice"
+        },
+        {
+            type: "confirm",
+            message: "Are you sure:",
+            name: "confirm",
+            default: true
+        }
+    ])
+    .then((response) => {
+        if (response.confirm) {
+            switch (response.choice) {
+                case "Get cards":
+                    break;
+                case "Create Basic Card":
+                    break;
+                case "Create Cloze Card":
+                    break;
+                case "Delete Cards":
+                    break;
+            }
+        } else {
+            inquireCommand();
+        }
+    })
+    .catch((error) => {
+        throw error;
+    })
+}
 
 // app.get('/listCards', function (request, response) {
 //     fs.readFile( __dirname + "/" + "cards.json", 'utf8', function (err, data) {
@@ -57,27 +113,6 @@ const georgeCloze = ClozeCard("George Washington was old", "George Washington");
 //         res.end( data );
 //     });
 // });
-
-function getCards() {
-    fs.readFile("cards.json", 'utf8', function (err, data) {
-        console.log( data );
-        // res.end( data );
-    });
-}
-
-function setCard(card) {
-    fs.readFile( "cards.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        console.log(data);
-        data.cloze_cards.push(card);
-        // console.log(data.length);
-        fs.writeFile( "cards.json", JSON.stringify(data));
-        // console.log( data );
-        // res.end( JSON.stringify(data));
-    });
-}
-
-
 
 // app.post('/addCard', function (request, response) {
 //     // First read existing cards.
@@ -98,5 +133,3 @@ function setCard(card) {
 
 // });
 
-console.log(georgeCloze);
-setCard(georgeCloze);
